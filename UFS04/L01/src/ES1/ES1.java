f15dffaaff815708ce95473bf0f9765e176b109b
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2026-01-09 15:36:57 +0100

    commit

--- UFS04/L01/src/ES1/ES1.java
+++ UFS04/L01/src/ES1/ES1.java
@@ -17,6 +17,9 @@ public class ES1 {
         System.out.print("C: ");
         float n3 = scanner.nextFloat();
 
+        System.out.println("La somma dei tre numeri è: " + (n1+n2+n3));
         System.out.println("La media dei tre numeri è: " + (n1+n2+n3)/3);
+
+        scanner.close();
     }
 }
