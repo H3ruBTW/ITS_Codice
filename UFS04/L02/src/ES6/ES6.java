457e4ac4814a18c58bb0be0e117d3e5c5ec948ee
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2026-01-20 17:17:50 +0100

    commit

--- UFS04/L02/src/ES6/ES6.java
+++ UFS04/L02/src/ES6/ES6.java
@@ -10,25 +10,28 @@ public class ES6 {
 
         String parola = scanner.nextLine().toLowerCase();
         String subString1, subString2;
-
-        if(parola.length()%2==0){
-            subString1 = parola.substring(0, parola.length()/2);
-            subString2 = parola.substring(parola.length()/2, parola.length());
-        }else{
-            subString1 = parola.substring(0, parola.length()/2);
-            subString2 = parola.substring(parola.length()/2+1, parola.length());
-        }
-
-        //Stampa di controllo
-        System.out.println(subString1 + " " + subString2);
-
         boolean pal = true;
 
-        for (int i = 0; i < subString1.length(); i++) {
-            if(subString1.charAt(i) != subString2.charAt(subString2.length()-i-1)){
-                pal = false; 
+        if(parola.length() > 1){
+            if(parola.length()%2==0){
+                subString1 = parola.substring(0, parola.length()/2);
+                subString2 = parola.substring(parola.length()/2, parola.length());
+            }else{
+                subString1 = parola.substring(0, parola.length()/2);
+                subString2 = parola.substring(parola.length()/2+1, parola.length());
             }
-        }
+
+            //Stampa di controllo
+            System.out.println(subString1 + " " + subString2);
+
+            for (int i = 0; i < subString1.length(); i++) {
+                if(subString1.charAt(i) != subString2.charAt(subString2.length()-i-1)){
+                    pal = false; 
+                    break;
+                }
+            }
+        } else 
+            pal = false;
 
         if(pal)
             System.out.println("La parola è palindroma");
