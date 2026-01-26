580969c0322ba32d4116171ccb2e4ee74768272c
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2026-01-26 17:21:13 +0100

    commit

--- UFS04/L03/src/ES2/ES2.java
+++ UFS04/L03/src/ES2/ES2.java
@@ -2,6 +2,27 @@ package ES2;
 
 public class ES2 {
     public static void main(String[] args) {
+        Studente studente2 = new Studente("Francesco");
+
+        studente2.NewVoto(0);
+        studente2.NewVoto(31);
+        studente2.NewVoto(25);
+        studente2.NewVoto(25);
+        studente2.NewVoto(25);
+        studente2.NewVoto(22);
+        studente2.NewVoto(22);
+        studente2.NewVoto(21);
+        studente2.NewVoto(18);
+
+        System.out.println("Nome: " + studente2.getNome());
+        System.out.println("Media: " + studente2.media());
+        System.out.println("Voto Minimo: " + studente2.VMin());
+        System.out.println("Voto Massimo: " + studente2.VMax());
+        System.out.println("Voto Mediano: " + studente2.mediana());
+        System.out.println("Promosso?: " + (studente2.isPromosso() ? "Si" : "No"));
+        System.out.println("Insufficienze: " + studente2.insufficienze());
+        System.out.println("Moda dei voti: " + studente2.moda());
+
         Studente studente = new Studente("Francesco");
 
         studente.NewVoto(0);
