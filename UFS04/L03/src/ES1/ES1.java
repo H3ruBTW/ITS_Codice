e570d4cfd4f5415cdf829e09addf9506489eca15
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2026-01-26 15:04:17 +0100

    commit

---
+++ UFS04/L03/src/ES1/ES1.java
@@ -0,0 +1,13 @@
+package ES1;
+
+public class ES1 {
+    public static void main(String[] args) {
+        Libro libro1 = new Libro("J.K. Rowling", "Harry Potter", 450, 40.50, 1980);
+
+        System.out.println("Valore per pagina: " + libro1.VPP());
+        System.out.println("Età: " + libro1.EtaLibro());
+        System.out.println("Vintage?: " + (libro1.isVintage() ? "Si" : "No"));
+        System.out.println("Sconto: " + libro1.Sconto(25.0));
+        libro1.Lunghezza();
+    }
+}
---
+++ UFS04/L03/src/ES1/Libro.java
@@ -0,0 +1,84 @@
+package ES1;
+
+public class Libro {
+    private String Autore, Titolo;
+    private Integer NPag, AnnoP;
+    private double Prezzo;
+
+    public String getAutore() {
+        return Autore;
+    }
+
+    public void setAutore(String Autore) {
+        this.Autore = Autore;
+    }
+
+    public String getTitolo() {
+        return Titolo;
+    }
+
+    public void setTitolo(String Titolo) {
+        this.Titolo = Titolo;
+    }
+    
+    public Integer getNPag() {
+        return NPag;
+    }
+
+    public void setNPag(Integer NPag) {
+        this.NPag = NPag;
+    }
+
+    public Integer getAnnoP() {
+        return AnnoP;
+    }
+
+    public void setAnnoP(Integer AnnoP) {
+        this.AnnoP = AnnoP;
+    }
+
+    public double getPrezzo() {
+        return Prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        Prezzo = prezzo;
+    }
+
+    public Libro(String Autore, String Titolo, Integer NPag, double Prezzo, Integer AnnoP){
+        this.Autore = Autore;
+        this.Titolo = Titolo;
+        this.NPag = NPag;
+        this.Prezzo = Prezzo;
+        this.AnnoP = AnnoP;
+    }
+
+    public double VPP(){
+        return (double)NPag/Prezzo;
+    }
+
+    public Integer EtaLibro(){
+        return 2026-AnnoP;
+    }
+
+    public Boolean isVintage(){
+        if(EtaLibro()>=50)
+            return true;
+       
+        return false;
+    }
+
+    public void Lunghezza(){
+        if(NPag<200)
+            System.out.println("Libro corto");
+        else if(NPag>400)
+            System.out.println("Libro lungo");
+        else
+            System.out.println("Libro medio");
+    }
+
+    public double Sconto(double sconto){
+        //Sconto con solo due decimali
+        return Math.floor(Prezzo*((100-sconto)/100)*100)/100;
+    }
+}
