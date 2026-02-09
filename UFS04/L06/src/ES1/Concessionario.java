783862700fa332ee1c0b67dfb4d2307ae872c339
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2026-02-09 17:44:29 +0100

    commit

--- UFS04/L06/src/ES1/Concessionario.java
+++ UFS04/L06/src/ES1/Concessionario.java
@@ -3,10 +3,15 @@ package ES1;
 un prezzo e un anno d'immatricolazione, tutte e due devono pagare l'assicurazione (metodo per calcolarla),
 per calcolare per le auto si parte da 500€ (suv: +10%, sportiva: +20%) per le moto parte da 300€ (cilindrata <125: +20%, 
 cilindrata >1000: +20%), il prezzo cambia per le auto (elettriche: -25% fino ad un massimo 5000€)
-Sulla concessionaria si può aggiungere i veicoli, ricerca per prezzo, vendita auto (salvarle)(veicolo, data, prezzo), 
+Sulla concessionaria si può aggiungere i veicoli, ricerca per prezzo tipo e assicurazione, vendita auto (salvarle)(veicolo, data, prezzo), 
 stampa vendite
+fatturato di uno specifico giorno o in un range
 */
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -66,16 +71,52 @@ public class Concessionario {
         }
     }
 
-    public void ricercaAutoPerPrezzo(double prIniziale, double prFinale){
+    public void ricercaAuto(double prIniziale, double prFinale, String tipo, double asIniziale, double asFinale){
         System.out.println("Veicoli trovati");
         Iterator<Veicolo> iterator = Veicoli.iterator();
+
+        if(!tipo.equalsIgnoreCase("Auto") && !tipo.equalsIgnoreCase("Moto") && !tipo.equalsIgnoreCase("null"))
+            throw new IllegalArgumentException("Tipo non accettato");
 
         while (iterator.hasNext()) {
             Veicolo veicolo = iterator.next();
 
-            if(veicolo.getPrezzo() > prIniziale && veicolo.getPrezzo() < prFinale){
+            if(prIniziale >= 0)
+                if(veicolo.getPrezzo() < prIniziale)
+                    continue;
 
-                if(veicolo instanceof Auto){
+            if(prFinale >= 0)
+                if(veicolo.getPrezzo() > prFinale)
+                    continue;
+
+            if(tipo.equalsIgnoreCase("null"))
+                continue;
+
+            if(tipo.equalsIgnoreCase("Auto")){
+                if(!(veicolo instanceof Auto))
+                    continue;
+                else {
+                    Auto auto = (Auto) veicolo;
+                    if(auto.getAssicurazione() < asIniziale)
+                        continue;
+
+                    if(auto.getAssicurazione() > asFinale)
+                        continue;
+                }
+            } else {
+                if(!(veicolo instanceof Moto))
+                    continue;
+                else {
+                    Moto moto = (Moto) veicolo;
+                    if(moto.getAssicurazione() < asIniziale)
+                        continue;
+
+                    if(moto.getAssicurazione() > asFinale)
+                        continue;
+                }
+            }
+
+            if(veicolo instanceof Auto){
                     Auto auto = (Auto) veicolo;
                     System.out.println("Tipo: Auto");
                     System.out.println("Carburante: " + auto.getCarburante());
@@ -86,13 +127,38 @@ public class Concessionario {
                     System.out.println("Tipo: Moto");
                     System.out.println("Cilindrata: " + moto.getCilindrata());
                     System.out.println("Assicurazione: " + moto.getAssicurazione());
-                }
+            }
 
-                System.out.println("Targa: " + veicolo.getTarga());
-                System.out.println("Anno Immatricolazione: " + veicolo.getAnnoImm());
-                System.out.println("Marca: " + veicolo.getMarca());
-                System.out.println("Prezzo: " + veicolo.getPrezzo());
+            System.out.println("Targa: " + veicolo.getTarga());
+            System.out.println("Anno Immatricolazione: " + veicolo.getAnnoImm());
+            System.out.println("Marca: " + veicolo.getMarca());
+            System.out.println("Prezzo: " + veicolo.getPrezzo());
+
+        }
+    }
+
+    public double getFatturatoGiorno(int gg, int MM, int YYYY){
+        double fatturato = 0;
+
+        LocalDate giorno = LocalDate.of(YYYY, MM, gg);
+        ZoneId fuso = ZoneId.of("Europe/Rome");
+
+        ZonedDateTime inizio = giorno.atStartOfDay(fuso);
+        ZonedDateTime fine = giorno.atTime(LocalTime.MAX).atZone(fuso);
+
+        Iterator<Vendita> iterator = Vendite.iterator();
+
+        while (iterator.hasNext()) {
+            Vendita vendita = iterator.next();
+
+            if(vendita.getData().isAfter(inizio) && vendita.getData().isBefore(fine)){
+                if(vendita.getVeicolo() instanceof Auto a)
+                    fatturato += a.getPrezzo();
+                else if(vendita.getVeicolo() instanceof Moto m)
+                    fatturato += m.getPrezzo();
             }
         }
+
+        return fatturato;
     }
 }
