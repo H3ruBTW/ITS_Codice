e18c9d20d88b98cc3dd2e01143ecd14b0604c6d2
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2026-04-21 15:39:07 +0200

    commit

--- UFS08/Project/assets/js/game.js
+++ UFS08/Project/assets/js/game.js
@@ -18,7 +18,9 @@ class Mazzo {
     }
 
     pesca() {
-        if (this.isEmpty) throw new Error("Mazzo vuoto!");
+        if (this.isEmpty) 
+            throw new Error("Mazzo vuoto!");
+
         return this.carte.pop();         
     }
 
@@ -35,29 +37,103 @@ banco_carte = [];
 giocatore_carte = [];
 bot_carte = [];
 
-let player_starts = Math.random() >= 0.5
+let player_starts = Math.random() >= 0
+let player_turn = player_starts
+
+startGame()
 
 function startGame(){
     pescataDiInfraturno()
 
     for(let i=0; i<4; i++)
-        banco_carte.push(mazzo.pesca())
+        addCard(mazzo.pesca(), "banco_c")
+
+    if(player_turn)
+        turnoGiocatore()
+    else
+        turnoBot()
+
 }
 
 function pescataDiInfraturno(){
     if(player_starts){
         for(let i=0; i<3; i++){
-            giocatore_carte.push(mazzo.pesca())
-            bot_carte.push(mazzo.pesca())
+            addCard(mazzo.pesca(), "giocatore")
+            addCard(mazzo.pesca(), "bot")
         }
     } else {
         for(let i=0; i<3; i++){
-            bot_carte.push(mazzo.pesca())
-            giocatore_carte.push(mazzo.pesca())
+            addCard(mazzo.pesca(), "bot")
+            addCard(mazzo.pesca(), "giocatore")
         }
+    }
+
+    updateCounter()
+}
+
+function addCard (carta, contenitore) {
+
+    let div
+    const img = document.createElement("img");
+
+    switch (contenitore) {
+        case "giocatore":
+            img.src = "assets/images/carte/" + carta.valore + "_" + carta.seme + ".jpg"
+            img.alt = "Carta di valore " + carta.valore + " e di seme " + carta.seme
+            giocatore_carte.push(carta, true)
+            break;
+        case "bot":
+            img.src = "assets/images/carte/retro.jpg"
+            img.alt = "Retro carta"
+            bot_carte.push(carta, true)
+            break;
+        case "banco_c":
+            img.src = "assets/images/carte/" + carta.valore + "_" + carta.seme + ".jpg"
+            img.alt = "Carta di valore " + carta.valore + " e di seme " + carta.seme
+            banco_carte.push(carta, true)
+            break;
+    }
+
+    img.classList.add("carta")
+
+    document.getElementById(contenitore).appendChild(img)
+}
+
+function updateCounter(){
+    document.getElementById("counter").textContent = mazzo.size
+}
+
+function turnoGiocatore(){
+    let selectedCard = null
+    let button = document.getElementById("confirm")
+
+    let document_cards = document.querySelectorAll("#giocatore img")
+
+    for (const card of document_cards) {
+        card.classList.add("toplay")
+        card.addEventListener("click", () => {
+            if (selectedCard) {
+                selectedCard.classList.remove("selected")
+                if (selectedCard === card) {
+                    selectedCard = null
+                    button.disabled = true
+                    return
+                }
+            }
+
+            card.classList.add("selected")
+            selectedCard = card
+            button.disabled = false
+        })
     }
 }
 
+function turnoBot(){
 
+}
+
+function endOfTurns(){
+
+}
 
 
