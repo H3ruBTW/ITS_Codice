01e359dee8f2712835f1e81d43f8ec6807e33da7
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2026-05-15 10:48:06 +0200

    commit

---
+++ UFS15/esempio/src/test/java/it/itsrizzoli/esempio/EsempioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.esempio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsempioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
